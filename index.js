e5f6a783da5197013b25e8bc6b36bfe7ebf37247
unknown <alexanderowens1993@gmail.com>
2019-01-08 20:24:08 +0000

    Added Heroku Required port code

--- index.js
+++ index.js
@@ -8,6 +8,20 @@ const d_token = (process.env.BOT_TOKEN);
 const cat_token = (process.env.CAT_TOKEN);
 const CAT_API_URL = "https://api.thecatapi.com/"
 
+const cool = require('cool-ascii-faces')
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
+
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .get('/cool', (req, res) => res.send(cool()))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  
+
 client.on('ready', () => {
     console.log("Connected as " + client.user.tag)
 });
